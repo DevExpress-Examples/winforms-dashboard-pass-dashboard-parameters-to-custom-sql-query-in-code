e6e4b6b57cb999924eda3f6b0c6f5f561f76344e
DevExpressExampleBot <fakeEmail>
2023-02-08 14:44:38 +0400

    Source auto update [skip ci]

--- VB/Dashboard_ParametersAndCustomSQL/Form1.vb
+++ VB/Dashboard_ParametersAndCustomSQL/Form1.vb
@@ -1,37 +1,30 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DataAccess.ConnectionParameters
 Imports DevExpress.XtraEditors
 
 Namespace Dashboard_ParametersAndCustomSQL
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits XtraForm
 
         Public Sub New()
             InitializeComponent()
-
-            Dim dashboard As New Dashboard()
-
+            Dim dashboard As Dashboard = New Dashboard()
             ' Creates a new dashboard parameter.
-            Dim parameter1 As New DashboardParameter("Parameter1", GetType(String), "Beverages", _
-                                                     "Type in the category name:", True, Nothing)
+            Dim parameter1 As DashboardParameter = New DashboardParameter("Parameter1", GetType(String), "Beverages", "Type in the category name:", True, Nothing)
             dashboard.Parameters.Add(parameter1)
-
             ' Creates a connection to the Northwind database and selects required data 
             ' according to the parameter's value.
-            Dim nwParams As New Access97ConnectionParameters("..\..\Data\nwind.mdb","","")
-            Dim sqlProvider As New SqlDataProvider("NW", _
-                                      nwParams, _
-                                      "select * from SalesPerson where CategoryName = @Parameter1")
+            Dim nwParams As Access97ConnectionParameters = New Access97ConnectionParameters("..\..\Data\nwind.mdb", "", "")
+            Dim sqlProvider As SqlDataProvider = New SqlDataProvider("NW", nwParams, "select * from SalesPerson where CategoryName = @Parameter1")
             ' Creates a data source and adds it to the dashboard data sources' collection.
-            Dim dataSource As New DataSource("Data Source 1", sqlProvider)
+            Dim dataSource As DataSource = New DataSource("Data Source 1", sqlProvider)
             dashboard.DataSources.Add(dataSource)
-
             ' Creates a Grid dashboard item with three columns.
-            Dim grid As New GridDashboardItem()
+            Dim grid As GridDashboardItem = New GridDashboardItem()
             grid.Columns.Add(New GridDimensionColumn(New Dimension("CategoryName")))
             grid.Columns.Add(New GridDimensionColumn(New Dimension("ProductName")))
             grid.Columns.Add(New GridMeasureColumn(New Measure("Extended Price")))
-
             dashboard.Items.Add(grid)
             grid.DataSource = dataSource
             dashboardViewer1.Dashboard = dashboard
--- VB/Dashboard_ParametersAndCustomSQL/Program.vb
+++ VB/Dashboard_ParametersAndCustomSQL/Program.vb
@@ -1,22 +1,20 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.Skins
 
 Namespace Dashboard_ParametersAndCustomSQL
-    Friend NotInheritable Class Program
 
-        Private Sub New()
-        End Sub
+    Friend Module Program
 
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-            SkinManager.EnableFormSkins()
-            Application.EnableVisualStyles()
+        <STAThread>
+        Sub Main()
+            Call SkinManager.EnableFormSkins()
+            Call Application.EnableVisualStyles()
             Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
+            Call Application.Run(New Form1())
         End Sub
-    End Class
+    End Module
 End Namespace
